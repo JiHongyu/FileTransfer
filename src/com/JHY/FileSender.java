306e9e33a27e50967206f74d971e12ba9847d576
JiHy <hongyuj@hotmail.com>
2016-01-26 15:52:52 +0800

    hahaha

---
+++ src/com/JHY/FileSender.java
@@ -0,0 +1,7 @@
+package com.JHY;
+
+/**
+ * Created by JiHongyu on 2016/1/26.
+ */
+public class FileSender {
+}
---
+++ src/com/JHY/Main.java
@@ -0,0 +1,15 @@
+package com.JHY;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Hi.display();
+    }
+}
+
+class Hi{
+    public static void display(){
+        System.out.println("hello");
+    }
+}
